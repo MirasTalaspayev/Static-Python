19dca7aaadd6b6dc1da26eba2c247d4d06f54208
tmk01.12.00 <tmk01.12.00@gmail.com>
2023-03-28 12:04:45 +0600

    used FunctionCall in statements and expressions

--- src/ast_elements/FunctionCall.java
+++ src/ast_elements/FunctionCall.java
@@ -1,8 +1,28 @@
 package ast_elements;
 
+import java.util.List;
+
 public class FunctionCall extends ASTElement {
-      @Override
-      public String toString() {
-        return super.toString();
-      }
+
+    private String func_name;
+    private List<Expression> ex_list;
+    
+    public FunctionCall(String func_name, List<Expression> ex_list) {
+        this.func_name = func_name;
+        this.ex_list = ex_list;
+    }
+
+    public StringBuilder toString(int indent) {
+        String ind = IndentUtil.indentStr(indent);
+        StringBuilder sb = new StringBuilder();
+        sb.append(ind).append(this.func_name + "(");
+        int size = ex_list.size();
+        for (int i = 0; i < size - 1; i++) {
+            sb.append(ex_list.get(i)).append(", ");
+        }
+        if (size >= 1) {
+            sb.append(ex_list.get(size - 1) + ")\n");
+        }
+        return sb;
+    }
 }
