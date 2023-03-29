d1a1ccde12e4f8e764d8a84a15bffc3a275a863e
ilequeque <ilyas.shakhabdinov@gmail.com>
2023-03-29 13:22:03 +0600

    current tuple

---
+++ src/ast_elements/TupleExpression.java
@@ -0,0 +1,52 @@
+package ast_elements;
+
+import java.util.List;
+import java.util.Map;
+
+import SemanticAnalysis.SemanticAnalysisException;
+
+public class TupleExpression extends CollectionExpressions {
+    
+    private List<Expression> values;
+    
+    public TupleExpression(List<Expression> values)
+    {
+        this.values = values;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        int size = values.size();
+        sb.append("(");
+        for (int i = 0; i < size - 1; i++) {
+            sb.append(values.get(i)).append(", ");
+        }
+        if (size >= 1) {
+            sb.append(values.get(size - 1));
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+	@Override
+	public Type analyzeAndGetType(Map<String, Type> variable_Map, Map<String, FunctionDeclaration> func_Map) throws SemanticAnalysisException {
+		if (values.size() == 0) {
+            return collectionType;
+        }
+
+        elementsType = values.get(0).analyzeAndGetType(variable_Map, func_Map);
+        
+        for (int i = 0; i < values.size() - 1; i++) {
+            if (!elementsType.equals(values.get(i).analyzeAndGetType(variable_Map, func_Map))) {
+                throw new SemanticAnalysisException("type of " + values.get(i) + " does not match with " + elementsType);
+            }
+        }
+        return new TupleType(elementsType);
+	}
+
+    @Override
+    public int size() {
+        return values.size();
+    }
+}
--- src/ast_elements/TupleType.java
+++ src/ast_elements/TupleType.java
@@ -10,14 +10,24 @@ public class TupleType extends Type {
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder("tuple[");
+        StringBuilder sb = new StringBuilder("(");
         int size = sub_Types.size();
         for (int i = 0; i < size - 1; i++)
         {
             sb.append(sub_Types.get(i)).append(", ");
         }
-        sb.append(sub_Types.get(size - 1)).append("]");
+        sb.append(sub_Types.get(size - 1)).append(")");
         return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) 
+            return true;
+        
+        if (!(other instanceof ListType))
+            return false;
+        
+        return this.sub_Types.equals(((TupleType)other).sub_Types);
+    }
 }
