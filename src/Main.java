e28a41fa19a6d55c5df66862e535d23067f24361
Deko17 <57325957+Deko17@users.noreply.github.com>
2023-04-11 13:54:09 +0600

    test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 /* Es 1 - Ex 1 (Main) */
 import ast_elements.ProgramAST;
 import java_cup.runtime.Symbol;
@@ -5,29 +6,41 @@ import java_cup.runtime.Symbol;
 import java.io.*;
 
 import SemanticAnalysis.SemanticAnalyzer;
-   
+
 public class Main {
-  @SuppressWarnings("deprecation")
-  static public void main(String argv[]) {
-    try {
-      /* Scanner instantiation */
-      Yylex l = new Yylex(new FileReader(".//example_expr.txt"));
-      /* Parser instantiation */
-      parser p = new parser(l);
-      /* Start the parser */
-      Object result = p.parse().value;
-      if (result instanceof ProgramAST) {
-        System.out.println("===================");
-        System.out.print(result.toString());
+	public static void processFiles(File folder) {
+		File[] files = folder.listFiles();
+		if (files == null) {
+			System.err.println("Error: Unable to read contents of directory " + folder.getAbsolutePath());
+			return;
+		}
+		for (File file : files) {
+			if (file.isDirectory()) {
+				processFiles(file);
+			} else if (file.isFile() && file.getName().endsWith(".spy")) {
+				try {
+					System.out.println("Processing file: " + file.getName());
+					Yylex l = new Yylex(new FileReader(file));
+					parser p = new parser(l);
+					Object result = p.parse().value;
+					if (result instanceof ProgramAST) {
+						System.out.println("===================");
+						System.out.print(result.toString());
 
-        System.out.println("=================== Phase3: Typecheck ===================");
-        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST)result);
-        semanticAnalyzer.analyze();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+						System.out.println("=================== Phase3: Typecheck ===================");
+						SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST) result);
+						semanticAnalyzer.analyze();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String folderPath = "../Before-After-Python/After";
+		File folder = new File(folderPath);
+		processFiles(folder);
+	}
 }
-
-
