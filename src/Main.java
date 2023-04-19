764a345f8ce2e3bb0a51bae824ed99b4ae235081
Deko17 <57325957+Deko17@users.noreply.github.com>
2023-04-19 12:23:30 +0600

    test2

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,39 @@ import java.io.*;
 
 import SemanticAnalysis.SemanticAnalyzer;
 
-public class Main {
-	public static void processFiles(File folder) {
-		File[] files = folder.listFiles();
-		if (files == null) {
-			System.err.println("Error: Unable to read contents of directory " + folder.getAbsolutePath());
-			return;
-		}
-		for (File file : files) {
-			if (file.isDirectory()) {
-				processFiles(file);
-			} else if (file.isFile() && file.getName().endsWith(".spy")) {
-				try {
-					System.out.println("Processing file: " + file.getName());
-					Yylex l = new Yylex(new FileReader(file));
-					parser p = new parser(l);
-					Object result = p.parse().value;
-					if (result instanceof ProgramAST) {
-						System.out.println("===================");
-						System.out.print(result.toString());
+public static void processFolder(String folderPath) {
+	File folder = new File(folderPath);
+	processFiles(folder);
+}
 
-						System.out.println("=================== Phase3: Typecheck ===================");
-						SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST) result);
-						semanticAnalyzer.analyze();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+public static void processFile(String filePath) {
+	File file = new File(filePath);
+	if (file.isFile() && file.getName().endsWith(".spy")) {
+		 try {
+			  System.out.println("Processing file: " + file.getName());
+			  Yylex l = new Yylex(new FileReader(file));
+			  parser p = new parser(l);
+			  Object result = p.parse().value;
+			  if (result instanceof ProgramAST) {
+					System.out.println("===================");
+					System.out.print(result.toString());
 
-	public static void main(String[] args) {
-		String folderPath = "../Before-After-Python/After";
-		File folder = new File(folderPath);
-		processFiles(folder);
+					System.out.println("=================== Phase3: Typecheck ===================");
+					SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST) result);
+					semanticAnalyzer.analyze();
+			  }
+		 } catch (Exception e) {
+			  e.printStackTrace();
+		 }
+	} else {
+		 System.err.println("Error: Invalid file or extension. Only .spy files are accepted.");
 	}
+}
+
+public static void main(String[] args) {
+	String folderPath = "../Before-After-Python/After";
+	processFolder(folderPath);
+
+	String filePath = "example_expr.txt";
+	processFile(filePath);
 }
