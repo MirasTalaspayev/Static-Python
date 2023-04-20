81d1976675921ffa0adec2c3c8197ac99095451a
tmk01.12.00 <tmk01.12.00@gmail.com>
2023-04-20 16:30:50 +0600

    changed CollectionType's getter method

--- src/ast_elements/CollectionType.java
+++ src/ast_elements/CollectionType.java
@@ -2,7 +2,7 @@ package ast_elements;
 
 public class CollectionType extends Type {
     protected Type elements_Type;
-    public Type getElementsType() {
+    public Type getElements_Type() {
         return elements_Type;
     }
 }
