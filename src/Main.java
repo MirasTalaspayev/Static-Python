a8b92c0bfadbc632f586a82808e6120c6d0fdc62
Deko17 <57325957+Deko17@users.noreply.github.com>
2023-04-20 19:47:56 +0600

    test3

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,40 @@ import java.io.*;
 
 import SemanticAnalysis.SemanticAnalyzer;
    
-public class Main {
-  @SuppressWarnings("deprecation")
-  static public void main(String argv[]) {
-    try {
-      /* Scanner instantiation */
-      Yylex l = new Yylex(new FileReader(".//example_expr.txt"));
-      /* Parser instantiation */
-      parser p = new parser(l);
-      /* Start the parser */
-      Object result = p.parse().value;
-      if (result instanceof ProgramAST) {
-        System.out.println("===================");
-        System.out.print(result.toString());
 
-        System.out.println("=================== Phase3: Typecheck ===================");
-        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST)result);
-        semanticAnalyzer.analyze();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+public static void processFolder(String folderPath) {
+	File folder = new File(folderPath);
+	processFiles(folder);
 }
 
+public static void processFile(String filePath) {
+	File file = new File(filePath);
+	if (file.isFile() && file.getName().endsWith(".spy")) {
+		 try {
+			  System.out.println("Processing file: " + file.getName());
+			  Yylex l = new Yylex(new FileReader(file));
+			  parser p = new parser(l);
+			  Object result = p.parse().value;
+			  if (result instanceof ProgramAST) {
+					System.out.println("===================");
+					System.out.print(result.toString());
 
+					System.out.println("=================== Phase3: Typecheck ===================");
+					SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST) result);
+					semanticAnalyzer.analyze();
+			  }
+		 } catch (Exception e) {
+			  e.printStackTrace();
+		 }
+	} else {
+		 System.err.println("Error: Invalid file or extension. Only .spy files are accepted.");
+	}
+}
+
+public static void main(String[] args) {
+	String folderPath = "../Before-After-Python/After";
+	processFolder(folderPath);
+
+	String filePath = ".//example_expr.txt";
+	processFile(filePath);
+}
