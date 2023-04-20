474654139d04dcd0e3f6bd24268e5994f4095ad5
Deko17 <57325957+Deko17@users.noreply.github.com>
2023-04-20 19:53:14 +0600

    fixed main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 /* Es 1 - Ex 1 (Main) */
 import ast_elements.ProgramAST;
 import java_cup.runtime.Symbol;
@@ -5,41 +6,43 @@ import java_cup.runtime.Symbol;
 import java.io.*;
 
 import SemanticAnalysis.SemanticAnalyzer;
-   
 
-public static void processFolder(String folderPath) {
-	File folder = new File(folderPath);
-	processFiles(folder);
-}
+public class Main {
 
-public static void processFile(String filePath) {
-	File file = new File(filePath);
-	if (file.isFile() && file.getName().endsWith(".spy")) {
-		 try {
-			  System.out.println("Processing file: " + file.getName());
-			  Yylex l = new Yylex(new FileReader(file));
-			  parser p = new parser(l);
-			  Object result = p.parse().value;
-			  if (result instanceof ProgramAST) {
+	public static void processFolder(String folderPath) {
+		File folder = new File(folderPath);
+		processFiles(folder);
+	}
+
+	public static void processFile(String filePath) {
+		File file = new File(filePath);
+		if (file.isFile() && file.getName().endsWith(".spy")) {
+			try {
+				System.out.println("Processing file: " + file.getName());
+				Yylex l = new Yylex(new FileReader(file));
+				parser p = new parser(l);
+				Object result = p.parse().value;
+				if (result instanceof ProgramAST) {
 					System.out.println("===================");
 					System.out.print(result.toString());
 
 					System.out.println("=================== Phase3: Typecheck ===================");
 					SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer((ProgramAST) result);
 					semanticAnalyzer.analyze();
-			  }
-		 } catch (Exception e) {
-			  e.printStackTrace();
-		 }
-	} else {
-		 System.err.println("Error: Invalid file or extension. Only .spy files are accepted.");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.err.println("Error: Invalid file or extension. Only .spy files are accepted.");
+		}
 	}
-}
 
-public static void main(String[] args) {
-	String folderPath = "../Before-After-Python/After";
-	processFolder(folderPath);
+	public static void main(String[] args) {
+		String folderPath = "../Before-After-Python/After";
+		processFolder(folderPath);
 
-	String filePath = ".//example_expr.txt";
-	processFile(filePath);
+		String filePath = ".//example_expr.txt";
+		processFile(filePath);
+	}
 }
