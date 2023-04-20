17257c50ce7e0274eb5cc3b676a0aa88a4f08db0
Nadir Netaliyev <nadirnetaliyev@MacBook-Air-Nadir-2.local>
2023-04-20 19:33:09 +0600

    added if-else update

--- src/ast_elements/IfStatement.java
+++ src/ast_elements/IfStatement.java
@@ -8,9 +8,15 @@ public class IfStatement extends Statement {
     private Expression cond;
     private List<Statement> body;
 
-    public IfStatement(Expression cond, List<Statement> body) {
+    private List<ElifStatement> elif_stmts;
+
+    private ElseStatement e_stmt;
+
+    public IfStatement(Expression cond, List<Statement> body, List<ElifStatement> elif_stmts, ElseStatement e_stmt) {
         this.cond = cond;
         this.body = body;
+        this.elif_stmts = elif_stmts;
+        this.e_stmt = e_stmt;
         System.out.println("CONDITION === " + cond);
     }
 
@@ -21,14 +27,20 @@ public class IfStatement extends Statement {
         for (Statement stmt : this.body) {
             sb.append(stmt.toString(indent + 1));
         }
+        if(elif_stmts != null){
+            for(int i=0; i<elif_stmts.size(); i++){
+                sb.append(elif_stmts.get(i).toString(indent));
+            }
+        }
+        if(e_stmt != null){
+            sb.append(e_stmt.toString(indent));
+        }
         return sb;
     }
 
     @Override
     public void analyze(Map<String, Type> variable_Map, Map<String, FunctionDeclaration> func_Map) throws SemanticAnalysisException {
-        if (!(cond instanceof BooleanExpression)) {
-            throw new SemanticAnalysisException("It is not boolean");
-        }
+        cond.analyze(variable_Map, func_Map, BooleanExpression.TYPE);
         
         Map<String, Type> localVar_Map = new HashMap<String, Type>(variable_Map);
         Map<String, FunctionDeclaration> localFun_Map = new HashMap<String, FunctionDeclaration>(func_Map);
@@ -37,5 +49,17 @@ public class IfStatement extends Statement {
         }
         localVar_Map = null;
         localFun_Map = null;
+        if(elif_stmts != null){
+            for(int i=0; i<elif_stmts.size(); i++){
+                localVar_Map = new HashMap<String, Type>(variable_Map);
+                localFun_Map = new HashMap<String, FunctionDeclaration>(func_Map);
+                elif_stmts.get(i).analyze(localVar_Map, localFun_Map);
+            }
+        }
+        if(e_stmt != null){
+            localVar_Map = new HashMap<String, Type>(variable_Map);
+            localFun_Map = new HashMap<String, FunctionDeclaration>(func_Map);
+            e_stmt.analyze(localVar_Map, localFun_Map);
+        }
     }
 }
